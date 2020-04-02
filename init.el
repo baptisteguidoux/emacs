c6fc3908c0b95ceb4410e0e680f88dcf856f8c60
Baptiste Guidoux <Baptiste454@gmail.com>
2020-04-02 15:19:05 -0400

    Create init.el

---
+++ init.el
@@ -0,0 +1,33 @@
+;; Dismiss start-up screen
+(setq inhibit-startup-screen t)
+
+;; Line number on the side
+(global-linum-mode t)
+
+;; MELPA config
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  )
+(package-initialize)
+
+
+;; List of packages
+(setq package-list 
+      '(autopair neotree doom-themes))
+
+;; Install missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+;; Theme
+(load-theme 'doom-nord-light)
